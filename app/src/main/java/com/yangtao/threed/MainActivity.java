420a283487299010bbc424165a72d98ec35d9ebd
张亮 <zhangliang01@weidian.com>
2017-12-06 17:27:40 +0800

    z

--- app/src/main/java/com/yangtao/threed/MainActivity.java
+++ app/src/main/java/com/yangtao/threed/MainActivity.java
@@ -10,8 +10,7 @@ import android.view.View;
 
 import com.yangtao.engine.Camera;
 import com.yangtao.engine.Surfaces;
-import com.yangtao.threed.element.Body;
-import com.yangtao.threed.element.Cube;
+import com.yangtao.threed.element.Rects;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,9 +20,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         MainView content = new MainView(this);
-        content.camera.scene.add(new Cube()
+        content.mCamera.mScene.add(new Rects(1, 1, 0.5f)
                 .setColor(Color.TRANSPARENT, Color.RED)
-                .setTranslate(5.5f, 0, Cube.UNIT / 2));
+                .setTranslate(5.5f, 0, 0.25f));
         setContentView(content);
     }
 }
@@ -31,22 +30,22 @@ public class MainActivity extends Activity {
 class MainView extends View implements Runnable {
     private static final float SHRINK = 1; //缩小常数
 
-    public MainCamera camera; //相机
+    public MainCamera mCamera; //相机
 
     public MainView(Context context) {
         super(context);
-        camera = new MainCamera((int) (getContext().getResources().getDisplayMetrics().widthPixels / SHRINK), (int) (getContext().getResources().getDisplayMetrics().heightPixels / SHRINK), 90);
+        mCamera = new MainCamera((int) (getContext().getResources().getDisplayMetrics().widthPixels / SHRINK), (int) (getContext().getResources().getDisplayMetrics().heightPixels / SHRINK), 90);
         post(this);
     }
 
     @Override
     public void run() {
-        for (Surfaces surfaces : camera.scene) {
-            if (surfaces instanceof Body) {
-                Body body = (Body) surfaces;
-                body.setRotate(body.mRotateH + 1, body.mRotateV + 1);
-            }
-        }
+//        for (Surfaces surfaces : mCamera.mScene) {
+//            if (surfaces instanceof Body) {
+//                Body body = (Body) surfaces;
+//                body.setRotate(body.mRotateH + 1, body.mRotateV + 1);
+//            }
+//        }
         postDelayed(this, 25);
     }
 
@@ -54,26 +53,26 @@ class MainView extends View implements Runnable {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.scale(SHRINK, SHRINK);
-        canvas.drawBitmap(camera.draw(), 0, 0, null);
+        canvas.drawBitmap(mCamera.draw(), 0, 0, null);
         postInvalidate();
     }
 }
 
 class MainCamera extends Camera {
-    private Bitmap bitmap; //位图
-    public List<Surfaces> scene; //场景
+    private Bitmap mBitmap; //位图
+    public List<Surfaces> mScene; //场景
 
     public MainCamera(int width, int height, float verticalAngle) {
         super(width, height, verticalAngle);
-        bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
-        scene = new ArrayList<>();
+        mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+        mScene = new ArrayList<>();
         mLens.jumpTo(1.8f);
     }
 
     public Bitmap draw() {
         clear();
-        for (Surfaces surfaces : scene) draw(surfaces);
-        bitmap.setPixels(mCanvas, 0, mWidth, 0, 0, mWidth, mHeight);
-        return bitmap;
+        for (Surfaces surfaces : mScene) draw(surfaces);
+        mBitmap.setPixels(mCanvas, 0, mWidth, 0, 0, mWidth, mHeight);
+        return mBitmap;
     }
 }
