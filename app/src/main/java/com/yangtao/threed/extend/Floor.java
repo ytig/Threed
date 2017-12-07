bd61aa4c93640f986be21ee76013b9f80d4405d6
张亮 <zhangliang01@weidian.com>
2017-12-07 23:35:22 +0800

    z

--- app/src/main/java/com/yangtao/threed/extend/Floor.java
+++ app/src/main/java/com/yangtao/threed/extend/Floor.java
@@ -7,23 +7,27 @@ import com.yangtao.threed.element.Rect;
  * 地板
  */
 public class Floor extends Body {
-    private int mSize; //行列数
-    private Integer[] mColors; //颜色集
+    private int xSize; //列数
+    private int ySize; //行数
+    private int[] mColors; //颜色集
     private Rect mRect; //地板块
 
-    public Floor(int size, float length, Integer... colors) {
-        mSize = size;
-        mColors = colors;
-        mRect = new Rect(length, length);
+    public Floor(int size, float length, int... colors) {
+        this(size, size, length, length, colors);
+    }
+
+    public Floor(int xSize, int ySize, float xLength, float yLength, int... colors) {
+        this.xSize = xSize;
+        this.ySize = ySize;
+        this.mColors = colors;
+        this.mRect = new Rect(xLength, yLength);
     }
 
     @Override
     public Object getChildAt(int index) {
-        if (mColors.length > 0 && index >= 0 && index < mSize * mSize) {
-            int i = index / mSize;
-            int j = index % mSize;
-            mRect.setColor(mColors[(2 * (i * mSize + j)) % mColors.length], mColors[(2 * (i * mSize + j) + 1) % mColors.length]);
-            mRect.setTranslate((2 * i + 1 - mSize) * mRect.mLengthX / 2, (2 * j + 1 - mSize) * mRect.mLengthY / 2, 0);
+        if (mColors.length > 0 && index >= 0 && index < xSize * ySize) {
+            mRect.setColor(mColors[(2 * index) % mColors.length], mColors[(2 * index + 1) % mColors.length]);
+            mRect.setTranslate((2 * (index % xSize) + 1 - xSize) * mRect.mLengthX / 2, (2 * (index / xSize) + 1 - ySize) * mRect.mLengthY / 2, 0);
             return mRect;
         }
         return null;
